10859ab576978564c5d3be515e9d0a4b463f29de
Tudor Jude <judetudor@meta.com>
2023-11-10 05:45:19 -0800

    Add scripts and prefabs for SceneAPIPlacement in DepthAPI-BiRP
    Summary: Add/update scripts for the SceneAPIPlacement scene in the DepthAPI-BiRP sample
    
    Reviewed By: vasylbo
    
    Differential Revision: D51072196
    
    fbshipit-source-id: f59fbc8918148c56b03ba82269e2ff68a6e7aafb

---
+++ DepthAPI-BiRP/Assets/DepthAPISample/Scripts/Poster.cs
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * Licensed under the Oculus SDK License Agreement (the "License");
+ * you may not use the Oculus SDK except in compliance with the License,
+ * which is provided at the time of installation or download, or which
+ * otherwise accompanies this software in either electronic or hard copy form.
+ *
+ * You may obtain a copy of the License at
+ *
+ * https://developer.oculus.com/licenses/oculussdk/
+ *
+ * Unless required by applicable law or agreed to in writing, the Oculus SDK
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using com.meta.xr.depthapi.utils;
+using TMPro;
+using UnityEngine;
+
+namespace DepthAPISample
+{
+    [RequireComponent(typeof(OcclusionDepthBias))]
+    public class Poster : MonoBehaviour
+    {
+        public Action<Poster> OnHighlight;
+        [SerializeField] private GameObject _highlight;
+        [SerializeField] private TextMeshPro _biasText;
+        [SerializeField] private AudioClip _highlightAudio;
+        private OcclusionDepthBias _depthBiasComponent;
+        private AudioSource _audioSource;
+        private bool _isHighlit;
+
+        private void Awake()
+        {
+            _depthBiasComponent = GetComponent<OcclusionDepthBias>();
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void Highlight()
+        {
+            _highlight.SetActive(true);
+            _biasText.gameObject.SetActive(true);
+            _isHighlit = true;
+            if (_audioSource != null)
+            {
+                _audioSource.clip = _highlightAudio;
+                _audioSource.Play();
+            }
+            OnHighlight?.Invoke(this);
+        }
+
+        public void Unhighlight()
+        {
+            _biasText.gameObject.SetActive(false);
+            _highlight.SetActive(false);
+            _isHighlit = false;
+        }
+
+        public void AdjustDepthBias(float val)
+        {
+            if (!_isHighlit) return;
+            _depthBiasComponent.AdjustDepthBias(val);
+            _biasText.text = "Depth bias set to: " + _depthBiasComponent.DepthBiasValue.ToString();
+            Debug.Log("Adjusting depth bias value to: " + _depthBiasComponent.DepthBiasValue);
+        }
+    }
+}
---
+++ DepthAPI-BiRP/Assets/DepthAPISample/Scripts/WallViewController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace DepthAPISample
+{
+    public class WallViewController : MonoBehaviour
+    {
+        [SerializeField] private OVRInput.RawButton _wallsVisibilityToggleButton = OVRInput.RawButton.Y;
+        private bool _areWallsVisible;
+
+        private MeshRenderer _meshRenderer;
+        private Material _material;
+        void Start()
+        {
+            _meshRenderer = GetComponent<MeshRenderer>();
+            _material = _meshRenderer.material;
+            if (_material != null)
+                AdjustTextureTiling();
+        }
+
+        private void AdjustTextureTiling()
+        {
+            _material.mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.y);
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                _areWallsVisible = !_areWallsVisible;
+                SetWallsVisible(_areWallsVisible);
+            }
+            if (OVRInput.GetDown(_wallsVisibilityToggleButton))
+            {
+                _areWallsVisible = !_areWallsVisible;
+                SetWallsVisible(_areWallsVisible);
+            }
+        }
+
+        private void SetWallsVisible(bool isOn)
+        {
+            _meshRenderer.enabled = isOn;
+        }
+    }
+}
