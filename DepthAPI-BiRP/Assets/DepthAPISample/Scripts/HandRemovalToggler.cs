7656d7e721c9ec93362c47977912633cc7eb4ace
Tudor Jude <judetudor@meta.com>
2023-12-20 10:00:48 -0800

    Add hand removal sample scene in BiRP
    Summary: Add a sample scene to showcase the hands removal feature.
    
    Reviewed By: vasylbo
    
    Differential Revision: D51998574
    
    fbshipit-source-id: 4e02263884d1a5499b850803c90c3c7b81797114

---
+++ DepthAPI-BiRP/Assets/DepthAPISample/Scripts/HandRemovalToggler.cs
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * Licensed under the Oculus SDK License Agreement (the "License");
+ * you may not use the Oculus SDK except in compliance with the License,
+ * which is provided at the time of installation or download, or which
+ * otherwise accompanies this software in either electronic or hard copy form.
+ *
+ * You may obtain a copy of the License at
+ *
+ * https://developer.oculus.com/licenses/oculussdk/
+ *
+ * Unless required by applicable law or agreed to in writing, the Oculus SDK
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Meta.XR.Depth;
+using UnityEngine;
+
+namespace DepthAPISample
+{
+    public class HandRemovalToggler : MonoBehaviour
+    {
+        public Action<HandsRemovalStyle> OnHandsRemovalStyleChanged;
+        public enum HandsRemovalStyle
+        {
+            None = 0,
+            VirtualHandMask = 1
+        }
+
+        [SerializeField] private OVRInput.RawButton _handsRemovalStyleToggler = OVRInput.RawButton.X;
+        [SerializeField] private Renderer _leftHandVisuals;
+        [SerializeField] private Renderer _rightHandVisuals;
+        private EnvironmentDepthTextureProvider _depthTextureProvider;
+        private HandsRemovalStyle _occlStyle = HandsRemovalStyle.VirtualHandMask;
+        private int _occlustionType;
+
+        private void Awake()
+        {
+            _depthTextureProvider = GetComponent<EnvironmentDepthTextureProvider>();
+        }
+
+        private void Start()
+        {
+            _occlustionType = (int)_occlStyle;
+            SetHandsOcclusionStyle(_occlStyle);
+        }
+
+        void Update()
+        {
+            if (OVRInput.GetDown(_handsRemovalStyleToggler))
+            {
+                _occlustionType = ((_occlustionType + 1) % Enum.GetValues(typeof(HandsRemovalStyle)).Length);
+                _occlStyle = (HandsRemovalStyle)_occlustionType;
+                SetHandsOcclusionStyle(_occlStyle);
+            }
+        }
+
+        private void SetHandsOcclusionStyle(HandsRemovalStyle style)
+        {
+            switch (style)
+            {
+                case HandsRemovalStyle.None:
+                    _depthTextureProvider.RemoveHands(false);
+                    _leftHandVisuals.gameObject.SetActive(false);
+                    _rightHandVisuals.gameObject.SetActive(false);
+                    break;
+                case HandsRemovalStyle.VirtualHandMask:
+                    _depthTextureProvider.RemoveHands(true);
+                    _leftHandVisuals.gameObject.SetActive(true);
+                    _rightHandVisuals.gameObject.SetActive(true);
+                    break;
+            }
+
+            OnHandsRemovalStyleChanged?.Invoke(style);
+        }
+    }
+}
---
+++ DepthAPI-BiRP/Assets/DepthAPISample/Scripts/HandsStyleUiListener.cs
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * Licensed under the Oculus SDK License Agreement (the "License");
+ * you may not use the Oculus SDK except in compliance with the License,
+ * which is provided at the time of installation or download, or which
+ * otherwise accompanies this software in either electronic or hard copy form.
+ *
+ * You may obtain a copy of the License at
+ *
+ * https://developer.oculus.com/licenses/oculussdk/
+ *
+ * Unless required by applicable law or agreed to in writing, the Oculus SDK
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Meta.XR.Depth;
+using TMPro;
+using UnityEngine;
+
+namespace DepthAPISample
+{
+    public class HandsStyleUiListener : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _handsStyleText;
+
+        [SerializeField] private HandRemovalToggler _handsRemovalToggler;
+
+        [SerializeField] private OVRHand _ovrHand;
+
+        [SerializeField] private GameObject _useHandsPromptUi;
+        [SerializeField] private GameObject _inputInstructionsUi;
+
+        private void OnEnable()
+        {
+            _handsRemovalToggler.OnHandsRemovalStyleChanged += UpdateHandsStyleText;
+        }
+
+        private void OnDisable()
+        {
+            _handsRemovalToggler.OnHandsRemovalStyleChanged -= UpdateHandsStyleText;
+        }
+
+        private void Update()
+        {
+            if (_ovrHand != null)
+            {
+                _useHandsPromptUi.SetActive(!_ovrHand.IsTracked);
+                _inputInstructionsUi.SetActive(_ovrHand.IsTracked);
+            }
+        }
+
+        private void UpdateHandsStyleText(HandRemovalToggler.HandsRemovalStyle style)
+        {
+            _handsStyleText.text = $"Hands removal style: {style}";
+        }
+    }
+}
