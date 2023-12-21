b9fabe12fd9ea80fb9e826e925126289316e9104
Tudor Jude <judetudor@meta.com>
2023-12-21 03:10:16 -0800

    Fix PerObjectOcclusion sample scene
    Summary: Remove EnvironmentDepthOcclusionController from PerObjectOcclusion scene. Add a script that simply enables environment depth and disables global shader keywords for occlusions.
    
    Reviewed By: vasylbo
    
    Differential Revision: D52328911
    
    fbshipit-source-id: 9612961b9a1e24a95fb113e3ba725335e648d6f7

---
+++ DepthAPI-BiRP/Assets/DepthAPISample/Scripts/EnvironmentDepthEnabler.cs
@@ -0,0 +1,40 @@
+using Meta.XR.Depth;
+using UnityEngine;
+
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * Licensed under the Oculus SDK License Agreement (the "License");
+ * you may not use the Oculus SDK except in compliance with the License,
+ * which is provided at the time of installation or download, or which
+ * otherwise accompanies this software in either electronic or hard copy form.
+ *
+ * You may obtain a copy of the License at
+ *
+ * https://developer.oculus.com/licenses/oculussdk/
+ *
+ * Unless required by applicable law or agreed to in writing, the Oculus SDK
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace DepthAPISample
+{
+    [RequireComponent(typeof(EnvironmentDepthTextureProvider))]
+    public class EnvironmentDepthEnabler : MonoBehaviour
+    {
+        private void Start()
+        {
+            var depthTextureProvider = GetComponent<EnvironmentDepthTextureProvider>();
+            if (depthTextureProvider != null)
+            {
+                Shader.DisableKeyword(EnvironmentDepthOcclusionController.HardOcclusionKeyword);
+                Shader.DisableKeyword(EnvironmentDepthOcclusionController.SoftOcclusionKeyword);
+                depthTextureProvider.SetEnvironmentDepthEnabled(true);
+            }
+        }
+    }
+}
